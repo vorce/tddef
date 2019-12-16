afd092f4d8ceb16f9e41ef39e979278344e11c36
Joel C <joel@vorce.se>
2019-12-16 14:55:48 +0100

    Add hex.pm metadata
    Fixes #1

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Tddef.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -19,6 +21,20 @@ defmodule Tddef.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "tddef helps you do test driven development in Elixir"
+  end
+
+  defp package() do
+    [
+      maintainers: ["joel@vorce.se"],
+      licenses: ["LGPL-3.0"],
+      links: %{"GitHub" => "https://github.com/vorce/tddef"}
+    ]
   end
 end
